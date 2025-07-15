70663b0b38a327adb5fb89ae9e5d1694378fdc74
hbilu <hacerbilu@gmail.com>
2025-07-15 14:25:31 +0200

    Fix CI: grant hacer MySQL privileges and flush to enable test DB creation

--- assignment_project/assignment_project/settings.py
+++ assignment_project/assignment_project/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-52ha024&9bc)we&2dzsmg2-_2xf_-sjh*h8o@lplpgbgb)d-$7'
+# SECRET_KEY = 'django-insecure-52ha024&9bc)we&2dzsmg2-_2xf_-sjh*h8o@lplpgbgb)d-$7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
